3cf085f1b0c166f54a1dc1e17733df3ccda92f61
Anibal Dominguez <benjad971.bd@gmail.com>
2026-04-30 09:34:51 -0300

    Corregimos validaciones en cabaña

--- app/validators/cabana.ts
+++ app/validators/cabana.ts
@@ -2,7 +2,17 @@ import vine from '@vinejs/vine'
 
 
 export const validadorCabana = vine.create({
-    nombre: vine.string().trim().minLength(3).maxLength(100),
+    nombre: vine.string().trim().unique({
+        table: 'cabana',
+        column: 'nombre',
+        filter: (db, value, field) => {
+
+            if (field.meta.cabanaId) {
+                db.whereNot('id_cabana', field.meta.cabanaId)
+
+            }
+        }
+    }).minLength(3).maxLength(100),
     descripcion: vine.string().trim().maxLength(500),
     capacidad: vine.number().min(1).max(50),
     habitaciones: vine.number().min(1).max(10),
