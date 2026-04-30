18eae02dff4a48c7c031c5cb95e602b1d87629ba
Anibal Dominguez <benjad971.bd@gmail.com>
2026-04-30 09:34:26 -0300

    agregamos metodos para editar cabaña y implementamos gestion de imagenes

--- app/controllers/cabanas_controller.ts
+++ app/controllers/cabanas_controller.ts
@@ -2,6 +2,7 @@ import type { HttpContext } from '@adonisjs/core/http'
 import Cabana from '#models/cabana'
 import Servicio from '#models/servicio'
 import { validadorCabana } from '#validators/cabana'
+import stringHelpers from '@adonisjs/core/helpers/string'
 
 export default class CabanasController {
 
@@ -13,27 +14,116 @@ export default class CabanasController {
   }
 
   async agregarCabana({ request, response, session }: HttpContext) {
+
     const datos = await request.validateUsing(validadorCabana)
 
     const servicios = request.input('servicios[]', [])
 
-
-
-    const cabana = await Cabana.create({
-      nombre: datos.nombre,
-      descripcion: datos.descripcion,
-      capacidad: datos.capacidad,
-      habitaciones: datos.habitaciones,
-      precioPorNoche: datos.precio_por_noche,
-      idEstado: 1,
+    const img = request.file('imagen', {
+      size: '20mb',
+      extnames: ['jpg', 'png', 'jpeg', 'webp']
     })
 
-    if (servicios.length > 0) {
-      await cabana.related('servicios').attach(servicios)
+    try {
+      const cabana = await Cabana.create({
+        nombre: datos.nombre,
+        descripcion: datos.descripcion,
+        capacidad: datos.capacidad,
+        habitaciones: datos.habitaciones,
+        precioPorNoche: datos.precio_por_noche,
+        idEstado: 1,
+      })
+
+
+
+      if (img) {
+        await img.moveToDisk(`cabanas_img/${datos.nombre}.${img.extname}`)
+        cabana.imgUrl = `cabanas_img/${datos.nombre}.${img.extname}`
+        await cabana.save()
+      }
+
+      if (servicios.length > 0) {
+        await cabana.related('servicios').attach(servicios)
+      }
+
+      session.flash('success', 'Cabaña guardada correctamente')
+      return response.redirect().toRoute('cabanas')
+
+    } catch (error) {
+
+      session.flash(request.except(['imagen', '_csrf']))
+      session.flash('error', 'Error: Ocurrio un error al insertar la cabaña.')
     }
 
-    session.flash('notificacion', 'Cabaña guardada correctamente')
-    return response.redirect().toRoute('cabanas')
+    return response.redirect().back()
+
+
+  }
+
+  async editar({ params, view }: HttpContext) {
+
+    const cabana = await Cabana.query().where('slug', params.slug).preload('servicios').firstOrFail()
+
+    const servicios = await Servicio.all();
+
+    const serviciosActuales = cabana.servicios.map((servicio) => servicio.id)
+
+    return view.render('pages/cabanas/altaCabana', { cabana, servicios, serviciosActuales })
+  }
+
+  async actualizar({ response, request, session }: HttpContext) {
+
+    try {
+
+      const cabana = await Cabana.findOrFail(request.input('id'))
+
+      const img = request.file('imagen', {
+        size: '20mb',
+        extnames: ['jpg', 'png', 'jpeg', 'webp']
+      })
+
+      const nuevosDatos = await request.validateUsing(validadorCabana, {
+        meta: {
+          cabanaId: request.input('id')
+        }
+      })
+
+      const serviciosInput = request.input('servicios[]', [])
+
+      const servicios = await Servicio.all();
+
+      await cabana.merge({
+        //nombre: nuevosDatos.nombre,
+        descripcion: nuevosDatos.descripcion,
+        capacidad: nuevosDatos.capacidad,
+        habitaciones: nuevosDatos.habitaciones,
+        precioPorNoche: nuevosDatos.precio_por_noche,
+
+      }).save()
+
+      if (servicios.length > 0) {
+        await cabana.related('servicios').sync(serviciosInput)
+      }
+
+      if (img) {
+
+        const path = `cabanas_img/${stringHelpers.uuid()}.${img.extname}`
+
+        await img.moveToDisk(path)
+        cabana.imgUrl = path
+        await cabana.save()
+      }
+
+      return response.json(nuevosDatos)
+
+    } catch (error) {
+      session.flash(request.except(['imagen', '_csrf']))
+      session.flash('error', 'Error: Ocurrio un error al actualizar la cabaña.')
+      return response.redirect().back()
+    }
+
+
+
   }
 
   async listar({ view }: HttpContext) {
