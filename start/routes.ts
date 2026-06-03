10a430ec1f6cffe6b5786eed36d3b0507adabcb4
Anibal Dominguez <benjad971.bd@gmail.com>
2026-06-03 06:58:46 -0300

    Agregamos alias para algunas rutas

--- start/routes.ts
+++ start/routes.ts
@@ -54,12 +54,13 @@ const mockCabins = [
 
 router.on('/').render('pages/home').as('home')
 router.get('/cabanas', [controllers.Cabanas, 'listar']).as('cabanas')
-router.get('/gestion', [controllers.Cabanas, 'admin'])
+router.get('/gestion', [controllers.Cabanas, 'admin']).as('gestion')
 router.get('/cabana/:slug', [controllers.Cabanas, 'mostrar']).as('cabanas.mostrar')
 router.get('/cabanas/editar/:slug', [controllers.Cabanas, 'editar']).as('cabanas.editar')
 router.get('/cabanas/alta', [controllers.Cabanas, 'crear']).as('nuevaCabana')
 router.post('cabanas/guardar', [controllers.Cabanas, 'agregarCabana']).as('guardar')
 router.put('cabanas/modificar', [controllers.Cabanas, 'actualizar']).as('modificar')
+router.delete('cabanas/eliminar/:id', [controllers.Cabanas, 'eliminarCabana']).as('cabanas.eliminar')
 router.get('/register', [RegisterController, 'show'])
 router.post('/register', [RegisterController, 'store'])
 
