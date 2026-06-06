fe32afb3864fef2482f8befb35b5df070073dced
Anibal Dominguez <benjad971.bd@gmail.com>
2026-06-06 11:04:23 -0300

    Corregimos el nombre de la tabla

--- database/migrations/1776385994734_create_huesped_reservas_table.ts
+++ database/migrations/1776385994734_create_huesped_reservas_table.ts
@@ -1,7 +1,7 @@
 import { BaseSchema } from '@adonisjs/lucid/schema'
 
 export default class extends BaseSchema {
-  protected tableName = 'huesped_reservas'
+  protected tableName = 'huesped_reserva'
 
   async up() {
     this.schema.createTable(this.tableName, (table) => {
